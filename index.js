444f0e3931989e10e6bae0c24085ad01ee1d86f6
Md Minhaj <parsonal322532@gmail.com>
2024-01-12 22:31:13 +0600

    JWT Added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -21,6 +22,22 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    console.log('Hating Area');
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if(error){
+            return res.status(403).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -30,6 +47,14 @@ async function run() {
         const services = collectionsDB.collection('services');
         const checkoutDB = collectionsDB.collection('checkout');
 
+        // JWT 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+            res.send({token})
+        })
+
+        // Services
         app.get('/services', async (req, res) => {
             const cursor = await services.find().toArray();
             res.send(cursor)
@@ -45,7 +70,11 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/details', async(req, res) => {
+        app.get('/details', verifyJWT, async(req, res) => {
+            const decoded = req.decoded;
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({error: 1, message: 'forbidden access'})
+            }
             let query = {};
             if(req.query?.email){
                 query.email = req.query.email;
